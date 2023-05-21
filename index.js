8ad88afef9053c45920f4c8e268b30310f3e1f1e
khadijaBOUKIR <kh2007.boukir@gmail.com>
2023-05-21 17:47:16 +0200

    more validations

--- index.js
+++ index.js
@@ -17,18 +17,22 @@ function extractNumbersFromArray(table) {
 
 function returnedLargestNb(tableOfNumbers, table) {
     let theLargest = tableOfNumbers[0];
-    for (let i = 0; i < tableOfNumbers.length; i++) {
-        if (theLargest < tableOfNumbers[i])
-            theLargest = tableOfNumbers[i]
+    if (table.length !== 1) {
+        for (let i = 0; i < tableOfNumbers.length; i++) {
+            if (theLargest < tableOfNumbers[i])
+                theLargest = tableOfNumbers[i]
+        }
     }
     return returnedLargestID(table, theLargest)
 }
 
 function returnedLargestID(table, theLargest) {
     let id = table[0].id;
-    for (let k = 0; k < table.length; k++) {
-        if (table[k].nb === theLargest)
-            id = table[k].id
+    if (table.length !== 1) {
+        for (let k = 0; k < table.length; k++) {
+            if (table[k].nb === theLargest)
+                id = table[k].id
+        }
     }
     return id
 }
