83dec413d5905005258470c8e044ba0c6562162f
khadijaBOUKIR <kh2007.boukir@gmail.com>
2023-05-21 13:21:22 +0200

    modf

--- index.js
+++ index.js
@@ -3,23 +3,32 @@ function search(table) {
     for (let j = 0; j < table.length; j++) {
         theLargest[j] = table[j].nb;
     }
-    return returnedLargest(theLargest)
+    return returnedLargest(theLargest) // return nb
 }
 
-function returnedLargest(table) {
-    let nb = 0;
-    for (let i = 0; i < table.length; i++) {
-        if (table[i] > table[i + 1]) {
-            nb = table[0]
-        } else nb = table[1]
+function returnedLargest(tab) {
+    let nb = tab[0];
+    for (let i = 0; i < tab.length; i++) {
+        if (nb < tab[i])
+            nb = tab[i]
+
     }
-    return nb
+    return nb //?
 }
-console.log(
+console.log("the largest is :",
     search([
-        { id: 2, nb: 5 },
-        { id: 0, nb: 13 },
-        { id: 1, nb: 0 },
-        { id: 2, nb: 75 },
+        { id: 0, nb: 5 },
+        { id: 1, nb: 13 },
+        { id: 2, nb: 0 },
+        { id: 3, nb: 75 }
+    ])
+);
+
+console.log("the largest is :",
+    search([
+        { id: 0, nb: 105 },
+        { id: 1, nb: 13 },
+        { id: 2, nb: 0 },
+        { id: 3, nb: 75 }
     ])
 );
