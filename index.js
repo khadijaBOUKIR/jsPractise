cf47e3c858493ab143817baa33d388c31d1fd2b8
khadijaBOUKIR <kh2007.boukir@gmail.com>
2023-05-21 13:46:09 +0200

    modify

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-function search(table) {
+function GetPositionOfGreaterNumber(table) {
+    const theLargest = extractNumbersFromArray(table)
+    return returnedLargestNb(theLargest) // return nb
+}
+
+function extractNumbersFromArray(table) {
     let theLargest = [];
+
     for (let j = 0; j < table.length; j++) {
         theLargest[j] = table[j].nb;
     }
-    return returnedLargest(theLargest) // return nb
+    return theLargest
 }
 
-function returnedLargest(tab) {
+function returnedLargestNb(tab) {
     let nb = tab[0];
     for (let i = 0; i < tab.length; i++) {
         if (nb < tab[i])
             nb = tab[i]
-
     }
+    return returnedLargestID(theLargest) //?
+}
+
+function returnedLargestID(tableau) {
+    nb
     return nb //?
 }
 console.log("the largest is :",
-    search([
+    GetPositionOfGreaterNumber([
         { id: 0, nb: 5 },
         { id: 1, nb: 13 },
         { id: 2, nb: 0 },
@@ -25,7 +35,7 @@ console.log("the largest is :",
 );
 
 console.log("the largest is :",
-    search([
+    GetPositionOfGreaterNumber([
         { id: 0, nb: 105 },
         { id: 1, nb: 13 },
         { id: 2, nb: 0 },
